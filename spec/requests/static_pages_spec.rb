a1985cd03cfbfb4c174e743976de914eddc673cc
Ethan Webb <ashataka@gmail.com>
2014-05-09 18:33:44 -0600

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+		it "should have the title 'Home'" do
+			visit '/static_pages/home'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+		end
+  end
+
+  describe "Help Page" do
+	  it "should have the content 'No Help at this time'" do
+		  visit '/static_pages/help'
+		  expect(page).to have_content('No Help at this time')
+	  end
+		it "should have the title 'Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+		end
+  end
+
+  describe "About Page" do
+	  it "should have the content 'About Us'" do
+		  visit '/static_pages/about'
+		  expect(page).to have_content('About Us')
+	  end
+		it "Should have the title 'About Us'" do
+			visit '/static_pages/about'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+		end
+  end
+
+end
